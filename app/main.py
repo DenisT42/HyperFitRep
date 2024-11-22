8ff7000ba13f3f513253cb2b0975215f2e3dbd38
Denis <d.timokhov@student.xu-university.de>
2024-11-22 17:23:52 +0100

    added dashboard.html, exercises.html, workout.html, edited base.html, login.html, register.html

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from starlette.responses import HTMLResponse
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
-
+static =
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
@@ -22,6 +22,18 @@ async def index(request: Request):
 @app.get("/login", response_class=HTMLResponse)
 async def index(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
+
+@app.get("/dashboard", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("dashboard.html", {"request": request})
+
+@app.get("/workout", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("workout.html", {"request": request})
+
+@app.get("/exercises", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("exercises.html", {"request": request})
 
 
 
